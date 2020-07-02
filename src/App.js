351c66aabab9b2a98d3ecc6f9a442493f281aa7a
Thiago Augusto <dev.digitalfront@gmail.com>
2020-07-01 22:42:52 -0300

    basic route work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import Home from "../src/pages/Home"
+import Contato from "../src/pages/Contato"
 
 function App() {
   return (
-    <div className="App">
-      index page
-      <h1>teste</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contato">contato</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/contato">
+            <Contato />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
