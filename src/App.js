78a7f0c9b7d0913459c4ee8feb214ffbcf25ee2e
Thiago Augusto <dev.digitalfront@gmail.com>
2020-07-02 01:27:38 -0300

    add json data menu links and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import MenuLinks from "./data/menuLinks.json"
 import Home from "../src/pages/Home"
+import Gastronomia from "../src/pages/Gastronomia"
+import Ambientes from "../src/pages/Ambientes"
+import Eventos from "../src/pages/Eventos"
+import Registros from "../src/pages/Registros"
+import Localizacao from "../src/pages/Localizacao"
 import Contato from "../src/pages/Contato"
+
 
 function App() {
   return (
@@ -9,23 +16,35 @@ function App() {
       <div>
         <nav>
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/contato">contato</Link>
-            </li>
+            {MenuLinks && 
+              MenuLinks.map((item, i) => 
+                <li key={i}>
+                  <Link to={item.path}>{item.titulo}</Link>
+                </li>)
+            }
           </ul>
         </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/gastronomia">
+            <Gastronomia />
+          </Route>
+          <Route path="/ambientes">
+            <Ambientes />
+          </Route>
+          <Route path="/eventos">
+            <Eventos />
+          </Route>
+          <Route path="/registros">
+            <Registros />
+          </Route>
+          <Route path="/localizacao">
+            <Localizacao />
+          </Route>
           <Route path="/contato">
             <Contato />
-          </Route>
-          <Route path="/">
-            <Home />
           </Route>
         </Switch>
       </div>
---
+++ src/pages/Registros/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => {
+    return(
+        <div>
+            Registros page
+        </div>
+    )
+}
